8224bafac5bf89e510aed668ee09ce3cf1faa73f
deminearchiver <minearchiver@gmail.com>
2025-11-23 20:59:19 +0300

    Added dynamic_color_ffi stub package.

---
+++ dynamic_color_ffi/android/src/main/kotlin/dev/deminearchiver/dynamic_color_ffi/DynamicColor.kt
@@ -0,0 +1,9 @@
+package dev.deminearchiver.dynamic_color_ffi
+
+import androidx.annotation.Keep
+
+@Keep
+object DynamicColor {
+  @JvmStatic
+  fun sayHi(): String = "Hello world!"
+}
---
+++ dynamic_color_ffi/example/android/app/src/main/kotlin/dev/deminearchiver/dynamic_color_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.deminearchiver.dynamic_color_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
