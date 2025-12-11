a20d51d6078727c00e4923583938d7f2d0aca89e
deminearchiver <minearchiver@gmail.com>
2025-12-11 13:52:27 +0300

    Replaced dynamic_color_ffi with a published version.

--- dynamic_color_ffi/lib/dynamic_color_ffi.dart
+++
@@ -1,7 +0,0 @@
-library;
-
-export 'dynamic_color_ffi_platform_interface.dart';
-
-export 'src/dynamic_color_android.dart';
-export 'src/dynamic_color_default.dart';
-export 'src/dynamic_color.dart';
--- dynamic_color_ffi/lib/dynamic_color_ffi_platform_interface.dart
+++
@@ -1,4 +0,0 @@
-library;
-
-export 'src/dynamic_color_platform.dart';
-export 'src/types.dart';
