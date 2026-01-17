492f4585cc54c7df530353d27d8c534379cecd20
deminearchiver <minearchiver@gmail.com>
2026-01-17 14:12:05 +0300

    Updated list item to use new 3P library features. Made InverseCenterOptically public.

--- lib/components/list_item.dart
+++ lib/components/list_item.dart
@@ -1,53 +1,136 @@
 import 'package:materium/flutter.dart';
 
-class _InverseCenterOptically extends CenterOptically {
-  const _InverseCenterOptically({
-    super.key,
-    super.enabled,
-    super.corners,
-    super.maxOffsets,
-    super.textDirection,
-    super.child,
-  });
+class _ListItemStates implements SegmentedListItemStates {
+  const _ListItemStates({required this.isFirst, required this.isLast});
 
   @override
-  _RenderInverseCenterOptically createRenderObject(BuildContext context) =>
-      _RenderInverseCenterOptically(
-        enabled: enabled,
-        corners: corners,
-        maxOffsets: maxOffsets,
-        textDirection: textDirection ?? Directionality.maybeOf(context),
-      );
+  final bool isFirst;
 
   @override
-  void updateRenderObject(
-    BuildContext context,
-    _RenderInverseCenterOptically renderObject,
-  ) {
-    renderObject
-      ..enabled = enabled
-      ..corners = corners
-      ..maxOffsets = maxOffsets
-      ..textDirection = textDirection ?? Directionality.maybeOf(context);
-  }
+  final bool isLast;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      runtimeType == other.runtimeType &&
+          other is _ListItemStates &&
+          isFirst == other.isFirst &&
+          isLast == other.isLast;
+
+  @override
+  int get hashCode => Object.hash(runtimeType, isFirst, isLast);
 }
 
-class _RenderInverseCenterOptically extends RenderCenterOptically {
-  _RenderInverseCenterOptically({
-    super.enabled,
-    super.corners,
-    super.maxOffsets,
-    super.textDirection,
-    super.child,
+sealed class _InteractiveListItemStates extends _ListItemStates
+    implements InteractiveListItemStates {
+  const _InteractiveListItemStates({
+    required super.isFirst,
+    required super.isLast,
+  });
+
+  const factory _InteractiveListItemStates.enabled({
+    required bool isFirst,
+    required bool isLast,
+    bool isHovered,
+    bool isFocused,
+    bool isPressed,
+  }) = _InteractiveListItemEnabledStates;
+
+  const factory _InteractiveListItemStates.disabled({
+    required bool isFirst,
+    required bool isLast,
+  }) = _InteractiveListItemDisabledStates;
+
+  factory _InteractiveListItemStates.fromWidgetStates(
+    WidgetStates states, {
+    required bool isFirst,
+    required bool isLast,
+  }) {
+    states as StrictSet<WidgetState>;
+    return states.contains(.disabled)
+        ? .disabled(isFirst: isFirst, isLast: isLast)
+        : .enabled(
+            isFirst: isFirst,
+            isLast: isLast,
+            isHovered: states.contains(.hovered),
+            isFocused: states.contains(.focused),
+            isPressed: states.contains(.pressed),
+          );
+  }
+
+  bool get isDisabled;
+
+  bool get isHovered;
+
+  bool get isFocused;
+
+  bool get isPressed;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      runtimeType == other.runtimeType &&
+          other is _InteractiveListItemStates &&
+          isFirst == other.isFirst &&
+          isLast == other.isLast &&
+          isHovered == other.isHovered &&
+          isFocused == other.isFocused &&
+          isPressed == other.isPressed &&
+          isDisabled == other.isDisabled;
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    isFirst,
+    isLast,
+    isHovered,
+    isFocused,
+    isPressed,
+    isDisabled,
+  );
+}
+
+class _InteractiveListItemDisabledStates extends _InteractiveListItemStates
+    implements InteractiveListItemDisabledStates {
+  const _InteractiveListItemDisabledStates({
+    required super.isFirst,
+    required super.isLast,
   });
 
   @override
-  double getHorizontalPaddingCorrection(BorderRadius borderRadius) =>
-      -super.getHorizontalPaddingCorrection(borderRadius);
+  bool get isDisabled => true;
 
   @override
-  double getVerticalPaddingCorrection(BorderRadius borderRadius) =>
-      -super.getVerticalPaddingCorrection(borderRadius);
+  bool get isHovered => false;
+
+  @override
+  bool get isFocused => false;
+
+  @override
+  bool get isPressed => false;
+}
+
+class _InteractiveListItemEnabledStates extends _InteractiveListItemStates
+    implements InteractiveListItemEnabledStates {
+  const _InteractiveListItemEnabledStates({
+    required super.isFirst,
+    required super.isLast,
+    this.isHovered = false,
+    this.isFocused = false,
+    this.isPressed = false,
+  });
+
+  @override
+  bool get isDisabled => false;
+
+  @override
+  final bool isHovered;
+
+  @override
+  final bool isFocused;
+
+  @override
+  final bool isPressed;
 }
 
 enum ListItemControlAffinity { leading, trailing }
@@ -202,7 +285,7 @@ extension on _ListItemContainerScope? {
     Widget? child,
   }) {
     final _CenterOpticallyConstructor constructor = inverse
-        ? _InverseCenterOptically.new
+        ? InverseCenterOptically.new
         : CenterOptically.new;
     return constructor(
       key: key,
@@ -281,7 +364,7 @@ class _ListItemInteractionState extends State<ListItemInteraction> {
   bool _focused = false;
 
   _InteractiveListItemStates _resolveStates() {
-    final states = _statesController.value;
+    final states = _statesController.value as StrictSet<WidgetState>;
 
     final _InteractiveListItemStates result =
         widget.onTap == null && widget.onLongPress == null
@@ -289,30 +372,30 @@ class _ListItemInteractionState extends State<ListItemInteraction> {
         : .enabled(
             isFirst: false,
             isLast: false,
-            isHovered: states.contains(WidgetState.hovered),
+            isHovered: states.contains(.hovered),
             isPressed: _pressed,
             isFocused: _focused && !_pressed,
           );
 
     if (result.isDisabled) {
-      states.add(WidgetState.disabled);
+      states.add(.disabled);
     } else {
-      states.remove(WidgetState.disabled);
+      states.remove(.disabled);
     }
     if (result.isHovered) {
-      states.add(WidgetState.pressed);
+      states.add(.pressed);
     } else {
-      states.remove(WidgetState.pressed);
+      states.remove(.pressed);
     }
     if (result.isFocused) {
-      states.add(WidgetState.focused);
+      states.add(.focused);
     } else {
-      states.remove(WidgetState.focused);
+      states.remove(.focused);
     }
     if (result.isPressed) {
-      states.add(WidgetState.pressed);
+      states.add(.pressed);
     } else {
-      states.remove(WidgetState.pressed);
+      states.remove(.pressed);
     }
     return result;
   }
@@ -448,18 +531,15 @@ class _ListItemInteractionState extends State<ListItemInteraction> {
         ) ??
         listItemTheme.stateLayerOpacity;
 
-    final overlayColor = WidgetStateProperty.resolveWith((widgetStates) {
-      final resolvedStates = _InteractiveListItemStates.fromWidgetStates(
+    final overlayColor = MixedWidgetStateLayerColor<ListItemStates>.from(
+      (widgetStates) => _InteractiveListItemStates.fromWidgetStates(
         widgetStates,
         isFirst: states.isFirst,
         isLast: states.isLast,
-      );
-      final resolvedColor = stateLayerColor.resolve(resolvedStates);
-      final resolvedOpacity = stateLayerOpacity.resolve(resolvedStates);
-      return resolvedOpacity > 0.0
-          ? resolvedColor.withValues(alpha: resolvedColor.a * resolvedOpacity)
-          : resolvedColor.withAlpha(0);
-    });
+      ),
+      color: stateLayerColor,
+      opacity: stateLayerOpacity,
+    );
 
     return listItemContainerScope.buildCenterOptically(
       inverse: true,
@@ -647,134 +727,4 @@ class ListItemLayout extends StatelessWidget {
       ),
     );
   }
-}
-
-class _ListItemStates implements SegmentedListItemStates {
-  const _ListItemStates({required this.isFirst, required this.isLast});
-
-  @override
-  final bool isFirst;
-
-  @override
-  final bool isLast;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      runtimeType == other.runtimeType &&
-          other is _ListItemStates &&
-          isFirst == other.isFirst &&
-          isLast == other.isLast;
-
-  @override
-  int get hashCode => Object.hash(runtimeType, isFirst, isLast);
-}
-
-sealed class _InteractiveListItemStates extends _ListItemStates
-    implements InteractiveListItemStates {
-  const _InteractiveListItemStates({
-    required super.isFirst,
-    required super.isLast,
-  });
-
-  const factory _InteractiveListItemStates.enabled({
-    required bool isFirst,
-    required bool isLast,
-    bool isHovered,
-    bool isFocused,
-    bool isPressed,
-  }) = _InteractiveListItemEnabledStates;
-
-  const factory _InteractiveListItemStates.disabled({
-    required bool isFirst,
-    required bool isLast,
-  }) = _InteractiveListItemDisabledStates;
-
-  factory _InteractiveListItemStates.fromWidgetStates(
-    WidgetStates states, {
-    required bool isFirst,
-    required bool isLast,
-  }) => states.contains(WidgetState.disabled)
-      ? .disabled(isFirst: isFirst, isLast: isLast)
-      : .enabled(
-          isFirst: isFirst,
-          isLast: isLast,
-          isHovered: states.contains(WidgetState.hovered),
-          isFocused: states.contains(WidgetState.focused),
-          isPressed: states.contains(WidgetState.pressed),
-        );
-
-  bool get isDisabled;
-
-  bool get isHovered;
-
-  bool get isFocused;
-
-  bool get isPressed;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      runtimeType == other.runtimeType &&
-          other is _InteractiveListItemStates &&
-          isFirst == other.isFirst &&
-          isLast == other.isLast &&
-          isHovered == other.isHovered &&
-          isFocused == other.isFocused &&
-          isPressed == other.isPressed &&
-          isDisabled == other.isDisabled;
-
-  @override
-  int get hashCode => Object.hash(
-    runtimeType,
-    isFirst,
-    isLast,
-    isHovered,
-    isFocused,
-    isPressed,
-    isDisabled,
-  );
-}
-
-class _InteractiveListItemDisabledStates extends _InteractiveListItemStates
-    implements InteractiveListItemDisabledStates {
-  const _InteractiveListItemDisabledStates({
-    required super.isFirst,
-    required super.isLast,
-  });
-
-  @override
-  bool get isDisabled => true;
-
-  @override
-  bool get isHovered => false;
-
-  @override
-  bool get isFocused => false;
-
-  @override
-  bool get isPressed => false;
-}
-
-class _InteractiveListItemEnabledStates extends _InteractiveListItemStates
-    implements InteractiveListItemEnabledStates {
-  const _InteractiveListItemEnabledStates({
-    required super.isFirst,
-    required super.isLast,
-    this.isHovered = false,
-    this.isFocused = false,
-    this.isPressed = false,
-  });
-
-  @override
-  bool get isDisabled => false;
-
-  @override
-  final bool isHovered;
-
-  @override
-  final bool isFocused;
-
-  @override
-  final bool isPressed;
 }
--- lib/flutter.dart
+++ lib/flutter.dart
@@ -1051,3 +1051,53 @@ class CornersFilledInputBorder extends InputBorder {
   @override
   int get hashCode => Object.hash(runtimeType, borderSide, delegate, corners);
 }
+
+class InverseCenterOptically extends CenterOptically {
+  const InverseCenterOptically({
+    super.key,
+    super.enabled,
+    super.corners,
+    super.maxOffsets,
+    super.textDirection,
+    super.child,
+  });
+
+  @override
+  RenderInverseCenterOptically createRenderObject(BuildContext context) =>
+      RenderInverseCenterOptically(
+        enabled: enabled,
+        corners: corners,
+        maxOffsets: maxOffsets,
+        textDirection: textDirection ?? Directionality.maybeOf(context),
+      );
+
+  @override
+  void updateRenderObject(
+    BuildContext context,
+    RenderInverseCenterOptically renderObject,
+  ) {
+    renderObject
+      ..enabled = enabled
+      ..corners = corners
+      ..maxOffsets = maxOffsets
+      ..textDirection = textDirection ?? Directionality.maybeOf(context);
+  }
+}
+
+class RenderInverseCenterOptically extends RenderCenterOptically {
+  RenderInverseCenterOptically({
+    super.enabled,
+    super.corners,
+    super.maxOffsets,
+    super.textDirection,
+    super.child,
+  });
+
+  @override
+  double getHorizontalPaddingCorrection(BorderRadius borderRadius) =>
+      -super.getHorizontalPaddingCorrection(borderRadius);
+
+  @override
+  double getVerticalPaddingCorrection(BorderRadius borderRadius) =>
+      -super.getVerticalPaddingCorrection(borderRadius);
+}
