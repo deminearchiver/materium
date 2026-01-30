70193af696ba4900a5f84460a67689b4cc559fbf
deminearchiver <minearchiver@gmail.com>
2026-01-30 12:47:18 +0300

    Upgraded 3P dependencies.

--- lib/flutter.dart
+++ lib/flutter.dart
@@ -79,7 +79,6 @@ export 'assets/fonts.gen.dart';
 export 'components/combining_builder.dart';
 export 'components/inverse_center_optically.dart';
 export 'components/list_item.dart';
-export 'components/raw_material_app.dart';
 export 'i18n/i18n.dart';
 export 'theme/extended_color.dart';
 export 'theme/legacy.dart';
